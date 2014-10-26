ec8a114ea7f8c241a5ce507881f1320b97ffc708
Chris Maloney <voldrani@gmail.com>
2014-10-26 00:40:38 -0400

    Add JS to control image width

---
+++ js/mesh-gh-pages.js
@@ -0,0 +1,9 @@
+(function($) {
+  // Control the size of the RDF graph images.  Usually 75% is just about right,
+  // but the max allowed will be 800px
+  $('img.rdf-graph').each(function() {
+    var old_width = $(this).width();
+    var nice_width = old_width * 0.75;
+    $(this).width(nice_width > 800 ? 800 : nice_width);
+  });
+})(jQuery)
