b90e63168be81efbd3dfbdf31775a06982fce794
Chris Maloney <voldrani@gmail.com>
2014-10-26 00:47:06 -0400

    Fix JS with document ready

--- js/mesh-gh-pages.js
+++ js/mesh-gh-pages.js
@@ -1,9 +1,11 @@
 (function($) {
-  // Control the size of the RDF graph images.  Usually 75% is just about right,
-  // but the max allowed will be 800px
-  $('img.rdf-graph').each(function() {
-    var old_width = $(this).width();
-    var nice_width = old_width * 0.75;
-    $(this).width(nice_width > 800 ? 800 : nice_width);
-  });
-})(jQuery)
+    $(document).ready(function() {
+        // Control the size of the RDF graph images.  Usually 75% is just about right,
+        // but the max allowed will be 800px
+        $('img.rdf-graph').each(function() {
+          var old_width = $(this).width();
+          var nice_width = old_width * 0.75;
+          $(this).width(nice_width > 800 ? 800 : nice_width);
+        });
+    });
+})(jQuery);
