18f66ad49b4ce13a736003e5792fac78cff2e149
Chris Maloney <maloneyc@ncbi.nlm.nih.gov>
2014-10-28 21:26:54 -0400

    Minor update to concept page

--- js/mesh-gh-pages.js
+++ js/mesh-gh-pages.js
@@ -10,6 +10,12 @@
         });
     });
   */
+
+  /*
+    This function converts a <span class='invoke-sparql'> element in the source into a
+    hyperlink that will call the SPARQL engine to produce the results of the query found
+    in the sparql code block.  It assumes there's only one sparql code block on the page.
+   */
   $(document).ready(function () {
       var sparql_endpoint = 'http://jatspan.org:8890/sparql';
       var sparql_format_param = 'format=text%2Frdf%2Bn3';
@@ -19,6 +25,5 @@
       var q_enc = encodeURIComponent(query_block.text());
       var endpoint_url = sparql_endpoint + "?query=" + q_enc + '&' + sparql_format_param;
       query_ref.wrap("<a href='" + endpoint_url + "' target='_blank'/>");
-
   });
 })(jQuery);
