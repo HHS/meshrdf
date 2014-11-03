a7e9c5e30aaa53e91b1c9c2c4880638e9dad1082
Chris Maloney <maloneyc@ncbi.nlm.nih.gov>
2014-11-03 17:17:02 -0500

    Update FTP URL for XML download script

--- bin/fetch-mesh-xml.sh
+++ bin/fetch-mesh-xml.sh
@@ -2,12 +2,12 @@
 
 mkdir -p $MESHRDF_HOME/data
 
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/desc2014.dtd -O $MESHRDF_HOME/data/desc2014.dtd
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/desc2014.xml -O $MESHRDF_HOME/data/desc2014.xml
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/pa2014.dtd -O $MESHRDF_HOME/data/pa2014.dtd
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/pa2014.xml -O $MESHRDF_HOME/data/pa2014.xml
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/qual2014.dtd -O $MESHRDF_HOME/data/qual2014.dtd
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/qual2014.xml -O $MESHRDF_HOME/data/qual2014.xml
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/supp2014.dtd -O $MESHRDF_HOME/data/supp2014.dtd
-wget ftp://ftp.nlm.nih.gov/online/mesh/.xmlmesh/supp2014.xml -O $MESHRDF_HOME/data/supp2014.xml
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/desc2014.dtd -O $MESHRDF_HOME/data/desc2014.dtd
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/desc2014.xml -O $MESHRDF_HOME/data/desc2014.xml
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/pa2014.dtd -O $MESHRDF_HOME/data/pa2014.dtd
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/pa2014.xml -O $MESHRDF_HOME/data/pa2014.xml
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/qual2014.dtd -O $MESHRDF_HOME/data/qual2014.dtd
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/qual2014.xml -O $MESHRDF_HOME/data/qual2014.xml
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/supp2014.dtd -O $MESHRDF_HOME/data/supp2014.dtd
+wget ftp://ftp.nlm.nih.gov/online/mesh/2014/supp2014.xml -O $MESHRDF_HOME/data/supp2014.xml
 
