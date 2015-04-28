5766db9724e08ddf8aa2f79878ba362aadf944ad
Daniel Davis <daniel.davis@nih.gov>
2015-04-28 18:09:42 -0400

    Changes to parameterize the fetch and convert of MESH XML These changes also tolerate a $MESHRDF_HOME that includes spaces

--- bin/mesh-xml2rdf.sh
+++ bin/mesh-xml2rdf.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+#
 # This script will convert all of the MeSH XML to RDF, assuming that your machine has
 # enough memory and resources.
-# Another option is to use the Perl script convert-all.pl, which first chops up the
+#
+# Another option is to use the Perl script mesh-rdf2xml.pl, which first chops up the
 # huge XML files into manageable chunks, and then passes each chunk through XSLT
 # separately.
+#
 
 if [ -z "$MESHRDF_HOME" ]; then
     echo "Please define MESHRDF_HOME environment variable" 1>&2
@@ -18,18 +21,40 @@ fi
 # Can override default year with MESHRDF_YEAR environment variable
 YEAR=${MESHRDF_YEAR:-2014}
 
-# Use year to modify $MESHRDF_HOME/xslt/mesh-rdf-prefixes.ent
+# Can override default Mesh prefix and output file with MESHRDF_PREFIX environment variable
+PREFIX=${MESHRDF_PREFIX:-mesh/$YEAR}
+OUT=`echo $PREFIX | sed -e 's,/,,'`
+
+# substitute the prefix into the DTD entity file
+sed -e "s,MESHRDF_PATH,$PREFIX,g" xslt/mesh-rdf-prefixes.ent.template > xslt/mesh-rdf-prefixes.ent
+if [ $? -ne 0 ]; then 
+    echo "Error creating xslt/mesh-rdf-prefixes.ent from template" 1>&2
+    exit 1
+fi
 
 mkdir -p "$MESHRDF_HOME/out"
 
-java -Xmx2G -jar $SAXON_JAR -s:$MESHRDF_HOME/data/qual$YEAR.xml \
-    -xsl:xslt/qual.xsl > $MESHRDF_HOME/out/mesh$YEAR-dups.nt
+java -Xmx2G -jar $SAXON_JAR -s:"$MESHRDF_HOME/data/qual$YEAR.xml" \
+    -xsl:xslt/qual.xsl > "$MESHRDF_HOME/out/$OUT-dups.nt"
+if [ $? -ne 0 ]; then
+    echo "Error converting $MESHRDF_HOME/data/qual$YEAR.xml" 1>&2
+    exit 1
+fi
 
-java -Xmx2G -jar $SAXON_JAR -s:$MESHRDF_HOME/data/desc$YEAR.xml \
-    -xsl:xslt/desc.xsl >> $MESHRDF_HOME/out/mesh$YEAR-dups.nt
+java -Xmx2G -jar $SAXON_JAR -s:"$MESHRDF_HOME/data/desc$YEAR.xml" \
+    -xsl:xslt/desc.xsl >> "$MESHRDF_HOME/out/$OUT-dups.nt"
+if [ $? -ne 0 ]; then
+    echo "Error converting $MESHRDF_HOME/data/desc$YEAR.xml"
+    exit 1
+fi
 
-java -Xmx2G -jar $SAXON_JAR -s:$MESHRDF_HOME/data/supp$YEAR.xml \
-    -xsl:xslt/supp.xsl >> $MESHRDF_HOME/out/mesh$YEAR-dups.nt
 
-sort -u -T$MESHRDF_HOME/out $MESHRDF_HOME/out/mesh$YEAR-dups.nt > $MESHRDF_HOME/out/mesh$YEAR.nt
-gzip -c $MESHRDF_HOME/out/mesh$YEAR.nt > $MESHRDF_HOME/out/mesh$YEAR.nt.gz
+java -Xmx2G -jar $SAXON_JAR -s:"$MESHRDF_HOME/data/supp$YEAR.xml" \
+    -xsl:xslt/supp.xsl >> "$MESHRDF_HOME/out/$OUT-dups.nt"
+if [ $? -ne 0 ]; then
+    echo "Error converting $MESHRDF_HOME/data/supp$YEAR.xml"
+    exit 1
+fi
+
+sort -u -T"$MESHRDF_HOME/out" "$MESHRDF_HOME/out/$OUT-dups.nt" > "$MESHRDF_HOME/out/$OUT.nt"
+gzip -c "$MESHRDF_HOME/out/$OUT.nt" > "$MESHRDF_HOME/out/$OUT.nt.gz"
