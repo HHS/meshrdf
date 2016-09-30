fdc4e43e592c5aa1c64ceab51be29586573b1495
Dan Davis <daniel.davis@nih.gov>
2016-09-30 16:48:09 -0400

    Add tests for status controller and diagnostic labels controller.

---
+++ webui/src/test/java/gov/nih/nlm/lode/tests/LabelsDiagControllerTest.java
@@ -0,0 +1,71 @@
+package gov.nih.nlm.lode.tests;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+/**
+ * The underlying MeshStatus class has been covered with the MeshStatusTest.
+ * The point of this test is just to verify the request methods of the
+ * StatusController.
+ *
+ * @author davisda4
+ */
+@ContextConfiguration(locations={"classpath:spring-test-context.xml"})
+@WebAppConfiguration
+@Test(groups = "unit")
+public class LabelsDiagControllerTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeClass(alwaysRun=true)
+    public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void testDefaults() throws Exception {
+        mvc.perform(get("/labels"))
+            .andExpect(content().contentType("text/plain; charset=utf-8"))
+            .andExpect(content().string(containsString("T504747")))
+            .andExpect(content().string(containsString("Acébutolol")))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testValidParms() throws Exception {
+        mvc.perform(get("/labels?id=T504748&rel=meshv:prefLabel"))
+            .andExpect(content().contentType("text/plain; charset=utf-8"))
+            .andExpect(content().string(containsString("T504748")))
+            .andExpect(content().string(containsString("Acebutolol Hydrochloride")))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testValidatesId() throws Exception {
+        mvc.perform(get("/labels?id=../../etc/passwd"))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testValidatesRelation() throws Exception {
+        mvc.perform(get("/labels?id=T504748&rel=meshv:identifier"))
+            .andExpect(status().isBadRequest());
+    }
+}
