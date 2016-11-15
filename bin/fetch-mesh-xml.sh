625e40772beedcfac2a9f77723a8f44c57bbddee
Dan Davis <daniel.davis@nih.gov>
2016-11-15 16:44:10 -0500

    Update README and conversion scripts to use 2017.

--- bin/fetch-mesh-xml.sh
+++ bin/fetch-mesh-xml.sh
@@ -19,7 +19,7 @@ fi
 mkdir -p "$MESHRDF_HOME/data"
 
 # CAn override default year with MESHRDF_YEAR environment variable
-YEAR=${MESHRDF_YEAR:-2016}
+YEAR=${MESHRDF_YEAR:-2017}
 
 # Can override default URI with MESHRDF_URI environment variable
 URI=${MESHRDF_URI:-ftp://ftp.nlm.nih.gov/online/mesh/$YEAR}
