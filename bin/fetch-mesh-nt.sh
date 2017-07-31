1dd56b95d6f35698eba5789c3369bd4b7a1ebb93
Daniel Davis <daniel.davis@nih.gov>
2017-07-31 18:40:24 -0400

    Add a new script to download mesh.nt files for any specific year
    - This will need to be enhanced to grab other files that need to be synced,
      so that we can re-populate new QA and new production from ftp.

---
+++ bin/fetch-mesh-nt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+
+while getopts "h:y:u:" opt; do
+    case $opt in 
+        h) export MESHRDF_HOME=$OPTARG ;;
+        y) export MESHRDF_YEAR=$OPTARG ;;
+        u) export MESHRDF_URI=$OPTARG ;;
+        *) echo "Usage: $0 [-h meshrdf-home] [-y year] [-u uri] [-i]" 1>&2 ; exit 1 ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+if [ -z "$MESHRDF_HOME" ]; then
+    echo "Please define MESHRDF_HOME environment variable" 1>&2
+    exit 1
+fi
+
+mkdir -p "$MESHRDF_HOME/out"
+
+# CAn override default year with MESHRDF_YEAR environment variable
+YEAR=${MESHRDF_YEAR:-current}
+
+# Can override default URI with MESHRDF_URI environment variable
+
+if [[ $YEAR = 'current' ]]; then
+    OUTDIR=$MESHRDF_HOME/out
+    URI=${MESHRDF_URI:-ftp://ftp.nlm.nih.gov/online/mesh}
+    YEAR=""
+else
+    OUTDIR=$MESHRDF_HOME/out/$YEAR
+    URI=${MESHRDF_URI:-ftp://ftp.nlm.nih.gov/online/mesh/$YEAR}
+fi
+
+
+mkdir "$OUTDIR" >& /dev/null
+curl "$URI/mesh${YEAR}.nt.gz" -o "$OUTDIR/mesh${YEAR}.nt.gz"
+gunzip -c "$OUTDIR/mesh${YEAR}.nt.gz" > "$OUTDIR/mesh${YEAR}.nt"
+
