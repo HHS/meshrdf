25f4af1daeb9402f8e5b9d755f7f0e9544239206
Daniel Davis <daniel.davis@nih.gov>
2017-08-03 12:27:26 -0400

    Resolve failure to run

--- webui/src/test/java/gov/nih/nlm/lode/tests/LodeBaseTest.java
+++ webui/src/test/java/gov/nih/nlm/lode/tests/LodeBaseTest.java
@@ -71,6 +71,7 @@ public class LodeBaseTest extends SeleniumTest {
   public void shouldBeValidLinks(List<WebElement> links) {
     LinkChecker linkcheck = new LinkChecker(getCurrentBaseUrl());
     linkcheck.addRequestHeader("Accept", "text/html, text/plain, text/turtle");
+    linkcheck.addRequestHeader("Upgrade-Insecure-Requests", "1");
     for (WebElement link : links) {
       String tag = link.getTagName();
       String href = (tag.equalsIgnoreCase("script") || tag.equalsIgnoreCase("img") ? link.getAttribute("src") : link.getAttribute("href"));
