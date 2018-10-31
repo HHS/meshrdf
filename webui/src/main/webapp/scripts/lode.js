aa6401f092791a000fcd406a0d27c2d79a28f68e
Dan Davis <daniel.davis@nih.gov>
2018-10-31 14:24:46 -0400

    Update development for MeSH 2019

--- webui/src/main/webapp/scripts/lode.js
+++ webui/src/main/webapp/scripts/lode.js
@@ -350,6 +350,7 @@ function _buildSparqlPage(element) {
               .append("<option value='2016'>2016</option>")
               .append("<option value='2017'>2017</option>")
               .append("<option value='2018'>2018</option>")
+              .append("<option value='2018'>2019</option>")
               .on("change", _fixQueryYear)
       );
 
