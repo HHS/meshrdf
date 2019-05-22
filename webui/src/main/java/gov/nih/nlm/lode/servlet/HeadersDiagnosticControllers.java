f1dc4425061faf78c3944762d199527f4e23b178
Dan Davis <daniel.davis@nih.gov>
2019-05-22 15:25:54 -0400

    Move headers controller to /internalonly/headers

--- webui/src/main/java/gov/nih/nlm/lode/servlet/HeadersDiagnosticControllers.java
+++ webui/src/main/java/gov/nih/nlm/lode/servlet/HeadersDiagnosticControllers.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("headers")
+@RequestMapping("internalonly/headers")
 public class HeadersDiagnosticControllers {
 
     @RequestMapping(method = RequestMethod.GET, produces=MediaType.TEXT_HTML_VALUE)
