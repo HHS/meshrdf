ca00fa27dae91ee2a2430e8d2dbca0ef0cc45bee
Daniel Davis <daniel.davis@nih.gov>
2019-08-21 13:11:18 -0400

    Enhance CorsFilter to log a warning on unacceptable header
    When the CorsFilter receives a preflight request with an unacceptable
    header, log it as a warning.

--- webui/src/main/java/gov/nih/nlm/lode/servlet/CorsFilter.java
+++ webui/src/main/java/gov/nih/nlm/lode/servlet/CorsFilter.java
@@ -1,12 +1,17 @@
 package gov.nih.nlm.lode.servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -17,17 +22,44 @@ import org.springframework.web.filter.OncePerRequestFilter;
  */
 public class CorsFilter extends OncePerRequestFilter {
 
+   private Logger logger = LoggerFactory.getLogger(getClass());
+   private static String[] ALLOWED_HEADERS = new String[] {
+           "Cache-Control",
+           "Pragma",
+           "Origin",
+           "Authorization",
+           "Content-Type",
+           "X-Requested-With"
+   };
+   private static final String allowedHeaders = String.join(", ", ALLOWED_HEADERS);
+
    @Override
    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
-        response.addHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-        response.addHeader("Access-Control-Max-Age", "1800"); // 30 minutes
 
-        if (request.getMethod().equals("OPTIONS")) {
-            // CORS "pre-flight" request
-            response.setStatus(HttpServletResponse.SC_ACCEPTED);
-            return;
+       response.addHeader("Access-Control-Allow-Origin", "*");
+       response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
+       response.addHeader("Access-Control-Allow-Headers", allowedHeaders);
+       response.addHeader("Access-Control-Max-Age", "1800"); // 30 minutes
+
+       if (request.getMethod().equals("OPTIONS")) {
+           String v;
+           if ((v = request.getHeader("Access-Control-Request-Headers")) != null) {
+               Set<String> allowedHeaderSet = Arrays.stream(ALLOWED_HEADERS).collect(Collectors.toSet());
+               boolean missing = false;
+               String[] headers = v.split(", *");
+               for (int i = 0; i < headers.length; i++) {
+                   if (!allowedHeaderSet.contains(headers[i])) {
+                       missing = true;
+                       break;
+                   }
+               }
+               if (missing) {
+                   logger.warn(String.format("pre-flight request with Access-Control-Request-Headers: %s", v));
+               }
+           }
+           // CORS "pre-flight" request
+           response.setStatus(HttpServletResponse.SC_ACCEPTED);
+           return;
         }
 
         // Simple request
